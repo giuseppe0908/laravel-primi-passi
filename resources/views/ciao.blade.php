6fce65544ec52e00386320ac70d7c9d46d4cabb2
giuseppe <placidagiuseppe@gmail.com>
2021-05-11 16:15:51 +0200

    ...

---
+++ resources/views/ciao.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <header>
+      <nav>
+        <ul>
+          <li>ciao</li>
+          <li>bello</li>
+        </ul>
+      </nav>
+    </header>
+  </body>
+</html>
